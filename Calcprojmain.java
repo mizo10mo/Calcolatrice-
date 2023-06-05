a2c48ce3556a1188f80f934f3b60efbe8167f31a
mizo10mo <130472010+mizo10mo@users.noreply.github.com>
2023-06-05 13:15:15 +0200

    Add files via upload

---
+++ Calcprojmain.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.calcproj;
+
+/**
+ *
+ * @author mizo
+ */
+public class Calcproj {
+
+    public static void main(String[] args) {
+        new frmcalcproj().setVisible(true) ;  
+    }
+}
